234cf62bc04c5c0c9247ebd53ae68bf71510d126
Zehra <zehrabbit@gmail.com>
2022-03-30 21:42:52 +0200

    Fixing front end to be displayed

--- server.js
+++ server.js
@@ -80,14 +80,14 @@ require("./routes/cart/addToCart.js")(app);
 require("./routes/cart/removeFromCart.js")(app);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use("/api", function (req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use("/api", function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
